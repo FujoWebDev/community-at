3d6da0996e12caa7a10d15489436ff95983e3eb4
ginger <26461046+gingerchew@users.noreply.github.com>
2025-10-15 15:55:05 -0400

    initial setup for sidebar topics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightSidebarTopicsPlugin from "starlight-sidebar-topics";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,14 +15,19 @@ export default defineConfig({
           href: "https://github.com/FujoWebDev/community-at",
         },
       ],
+      plugins: [
+        starlightSidebarTopicsPlugin([
+          {
+            label: "Sociocracy",
+            link: '/sociacracy/',
+            items: [
+              // Each item here is one entry in the navigation menu.
+              { label: "Example Guide", slug: "sociocracy/example" },
+            ],
+          },
+        ])
+      ],
       sidebar: [
-        {
-          label: "Sociocracy",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "sociocracy/example" },
-          ],
-        },
         {
           label: "FujoWeb.dev & FujoGuide",
           autogenerate: { directory: "fujowebdev" },
@@ -30,7 +36,7 @@ export default defineConfig({
           label: "GitHub",
           autogenerate: { directory: "github" },
         },
-      ],
+      ]
     }),
   ],
 });
